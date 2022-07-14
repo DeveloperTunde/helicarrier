4fd244c926ea5fa2a18f8887f1bf59ccded614b2
Developer Tunde <developertunde@Tundes-MacBook-Pro.local>
2022-07-14 20:56:42 +0100

    adjusted

--- src/api/ApolloClient.js
+++ src/api/ApolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
 
 export const Client = new ApolloClient({
-    uri: 'http://localhost:4000/',
+    uri: 'https://helicarrier.devtunde.xyz/',
     cache: new InMemoryCache(),
    
   });
